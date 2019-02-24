563586215648010713f143ce3200b1cfe079255e
Robert Pedersen <rob@worldcrimeleague.org>
2019-02-24 15:30:12 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,11 @@
 # code the #greeting method here!
+
+puts "Please enter your name;"
+name = gets.strip
+greeting(name)
+
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
+  
